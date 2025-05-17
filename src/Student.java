a8c91fcc0cd16f5ceb30759190f95effe16bb6fb
wayne <anvar.kartakaev@gmail.com>
2025-05-17 16:52:22 +0500

    For checking

---
+++ src/Student.java
@@ -0,0 +1,18 @@
+import java.util.Objects;
+
+public class Student {
+    private String name;
+    private int age;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return name.equals(student.name) && age == student.age;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
